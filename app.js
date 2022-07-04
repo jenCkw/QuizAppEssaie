be459e245128e2b73e06f54585becba3782aa154
jenCkw <cikwaninejenny@gmail.com>
2022-07-04 22:41:17 +0100

    ajout fonction displayquestion

--- app.js
+++ app.js
@@ -15,4 +15,34 @@ class Quiz {
         }
         this.questionIndex++;
     }
+
+    isEnded() {
+        return this.questionIndex === this.questions.length;
 }
+}
+
+//create a question class
+
+class Question {
+    constructor(text, choices, answer) {
+        this.text = text;
+        this.choice = choices;
+        this.answer = answer;
+    }
+
+    isCorrectAnswer(choice) {
+        return this.answer === choice;
+    }
+}
+
+//display question
+function displayquestion() {
+    if(quiz.isEnded()) {
+        showScores();
+    } else {
+        // show question
+        let questioneElement = document.getElementById("question");
+        questioneElement.innerHTML = quiz.getQuestionIndex().text;
+    }
+}
+   
