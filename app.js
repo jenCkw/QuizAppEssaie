201bdd4de39c7db6bbfca94273c6c56840d306d6
jenCkw <cikwaninejenny@gmail.com>
2022-07-04 22:01:08 +0100

    ajout methode getQuestionIndex and methode guess

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ class Quiz {
         this.question = question;
         this.questionIndex = 0;
     }
+    
+    getQuestionIndex(){
+        return this.question[this.questionIndex];  
+    }
+
+    guess(answer) {
+        if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+            this.score++;
+        }
+        this.questionIndex++;
+    }
 }
